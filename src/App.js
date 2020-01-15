746a6b53d64cbaf0b34af90be42451a51d8c8642
Brian Schulte <bschulte@Brians-MacBook-Pro.local>
2020-01-15 11:33:50 -0500

    updated with feedback

--- src/App.js
+++ src/App.js
@@ -1,47 +1,51 @@
-import React, {useState} from 'react';
-import logo from './logo.svg';
-import companylogo from './kryptowire.png';
-import './App.css';
-import moment from 'moment';
-import { Button, Menu, MenuItem, Popover} from "@blueprintjs/core";
-import 'normalize.css/normalize.css';
-import '@blueprintjs/core/lib/css/blueprint.css';
-import '@blueprintjs/icons/lib/css/blueprint-icons.css';
-import {BrowserRouter as Router, Link, Switch, Route} from 'react-router-dom';
-import Login from './pages/Login';
-import Reminder from './pages/reminder';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import companylogo from "./kryptowire.png";
+import "./App.css";
+import moment from "moment";
+import { Button, Menu, MenuItem, Popover } from "@blueprintjs/core";
+import "normalize.css/normalize.css";
+import "@blueprintjs/core/lib/css/blueprint.css";
+import "@blueprintjs/icons/lib/css/blueprint-icons.css";
+import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Reminder from "./pages/reminder";
 
 function App() {
-  
-  
   return (
-    
-
     <Router>
       <nav className="bp3-navbar bp3-dark">
-      <div className="bp3-navbar-group bp3-align-right">
-      <Link to ='/home'><button className="bp3-button bp3-minimal bp3-icon-home">Home</button></Link>
-      <span className="bp3-navbar-divider"></span>
-      <Link to ='/users'><button className="bp3-button bp3-minimal bp3-icon-user"></button></Link>
-      <Link to ='/reminder'><button className="bp3-button bp3-minimal bp3-icon-notifications"></button></Link>
-      <Link to ='/settings'><button className="bp3-button bp3-minimal bp3-icon-cog"></button></Link>
-      </div>
-    </nav>
+        <div className="bp3-navbar-group bp3-align-right">
+          <Link to="/home">
+            <button className="bp3-button bp3-minimal bp3-icon-home">
+              Home
+            </button>
+          </Link>
+          <span className="bp3-navbar-divider"></span>
+          <Link to="/users">
+            <button className="bp3-button bp3-minimal bp3-icon-user"></button>
+          </Link>
+          <Link to="/reminder">
+            <button className="bp3-button bp3-minimal bp3-icon-notifications"></button>
+          </Link>
+          <Link to="/settings">
+            <button className="bp3-button bp3-minimal bp3-icon-cog"></button>
+          </Link>
+        </div>
+      </nav>
       <Switch>
-        <Route path="/" exact component = {Home}/>
-        <Route path="/users" component = {Login}/>
-        <Route path="/reminder" component = {Reminder}/>
-
+        <Route path="/" exact component={Home} />
+        <Route path="/users" component={Login} />
+        <Route path="/reminder" component={Reminder} />
       </Switch>
     </Router>
   );
-
 }
 
 const Home = () => (
   <div>
     <h1>Home Page</h1>
   </div>
-)
+);
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,64 +1,78 @@
-import React, {useState} from 'react';
-import moment from 'moment';
-import { Button, Menu, MenuItem, Popover, FormGroup, InputGroup, Label, setHotkeysDialogProps} from "@blueprintjs/core";
-import 'normalize.css/normalize.css';
-import '@blueprintjs/core/lib/css/blueprint.css';
-import '@blueprintjs/icons/lib/css/blueprint-icons.css';
-import {BrowserRouter as Router, Link, Redirect} from 'react-router-dom';
-import './Login.css';
-import companylogo from '../kryptowire.png'
+import React, { useState } from "react";
+import moment from "moment";
+import {
+  Button,
+  Menu,
+  MenuItem,
+  Popover,
+  FormGroup,
+  InputGroup,
+  Label,
+  setHotkeysDialogProps
+} from "@blueprintjs/core";
+import "normalize.css/normalize.css";
+import "@blueprintjs/core/lib/css/blueprint.css";
+import "@blueprintjs/icons/lib/css/blueprint-icons.css";
+import { BrowserRouter as Router, Link, Redirect } from "react-router-dom";
+import "./Login.css";
+import companylogo from "../kryptowire.png";
 
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-function Login(){
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+  function validateForm() {}
 
-
-    function validateForm(){
-        
-
+  function handleSubmit(event) {
+    event.preventDefault();
+    try {
+      if (username === "admin" && password === "admin") {
+        alert("success");
+      } else {
+        alert("invalid");
+      }
+    } catch (e) {
+      alert(e.message);
     }
+  }
 
-    function handleSubmit(event){
-        event.preventDefault();
-        try {
-            if (username === 'admin' && password ==='admin'){
-                alert('success');
-                
-            }
-            else{
-                alert('invalid');
-            } 
-        } catch(e){
-            alert(e.message)
-        }
-    }
+  return (
+    <Router>
+      <div className="login">
+        <img src={companylogo} className="company-logo" alt="companylogo" />
+        <h1 className="bp3-heading"> Kryptowire Employee Portal</h1>
+        <FormGroup>
+          <Label className="bp3-label">
+            Username
+            <InputGroup
+              className="bp3-input"
+              id="user-input"
+              type="text"
+              placeholder="Username"
+              value={username}
+              onChange={e => setUsername(e.target.value)}
+            />
+          </Label>
 
+          <Label className="bp3-label">
+            Password
+            <InputGroup
+              className="bp3-input"
+              id="password-input"
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+          </Label>
 
-    return(
-        <Router>    
-            <div className ='login'>
-                <img src={companylogo} className="company-logo" alt="companylogo" />
-                <h1 className='bp3-heading'> Kryptowire Employee Portal</h1>
-                <FormGroup>
-                    <Label className="bp3-label">
-                        Username
-                        <InputGroup className="bp3-input" id="user-input" type="text" placeholder="Username" value ={username} onChange={e=>setUsername(e.target.value)}/>
-                    </Label>
-                    
-                    <Label className="bp3-label">
-                        Password
-                        <InputGroup className="bp3-input" id="password-input" type="password" placeholder="Password" value={password} onChange={e=>setPassword(e.target.value)}/>
-                    </Label>
-                    
-                    <Button className='login-btn' onClick={e=>handleSubmit(e)}>Sign In</Button>
-                </FormGroup>
-            </div>
-
-
-        </Router>
-    )
-
+          <Button className="login-btn" onClick={e => handleSubmit(e)}>
+            Sign In
+          </Button>
+        </FormGroup>
+      </div>
+    </Router>
+  );
 }
 
 export default Login;
